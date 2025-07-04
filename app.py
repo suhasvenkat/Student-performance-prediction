d8bc97da400d005fbb3ac13b3efabf3bf4c1a496
Suhas Venkat <suhasvenkat265@gmail.com>
2025-07-04 21:47:50 +0530

    Production-specific changes

--- app.py
+++ app.py
@@ -33,3 +33,5 @@ def predict_datapoint():
             return f"Something went wrong: {e}", 500
 
     return render_template("home.html")
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
