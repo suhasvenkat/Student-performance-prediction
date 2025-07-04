efa9cb48987ed969b8ca29f7df6a1bc00b6054f3
Suhas Venkat <suhasvenkat265@gmail.com>
2025-07-04 21:33:32 +0530

    stable version of scikit

--- app.py
+++ app.py
@@ -37,5 +37,5 @@ def health():
     return "OK", 200
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
